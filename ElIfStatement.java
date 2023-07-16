6e47c5efd0f74ef7b9eed13d44542caa895591fb
bosstoner <bosstoner@gmail.com>
2023-07-16 11:08:02 +0200

    ElIfStatement

---
+++ ElIfStatement.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ElIfStatement {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String username = askQuestion("Enter your name: ", scanner);
+
+        if (!username.equals(""))
+            System.out.println("Hello " + username + "!");
+        else {
+            username = askQuestion("What is your name? ", scanner);
+            System.out.println("Hello " + username + ". Welcome!");
+        }
+        scanner.close();
+    }
+
+    public static String askQuestion(String prompt, Scanner scanner) {
+        System.out.print(prompt);
+        String input = scanner.nextLine();
+        return input;
+    }
+}
