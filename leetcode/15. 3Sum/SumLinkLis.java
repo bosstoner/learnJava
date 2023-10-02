a42833a28fbf09eb49e8f7a39b47b771131bf7d9
bosstoner <bosstoner@gmail.com>
2023-10-02 22:03:21 +0200

    reorder

--- leetcode/15. 3Sum/SumLinkLis.java
+++ leetcode/15. 3Sum/SumLinkLis.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 class SumLinkLis {
 
     //2 pointers
--- leetcode/49. Group Anagrams/GroupAnagrams.java
+++ leetcode/49. Group Anagrams/GroupAnagrams.java
@@ -1,6 +1,12 @@
 /*Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once. */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
